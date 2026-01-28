6d5720979e1543b2de4f3aebe9b4a1d4bfb71cde
IsraelSoares0 <israel.soares.cf@gmail.com>
2026-01-27 21:18:34 -0300

    atividade 1 finalizada

--- Semana1/Questao1/atividade1.cpp
+++ Semana1/Questao1/atividade1.cpp
@@ -18,9 +18,12 @@ struct Linha {
     vector<tuple<int, int>> percurso; // (indice array, indice original)
     int tamanho;
     int soma_viabilidade = 0;
-    double media_viabilidade = 0;
+    double media_viabilidade = 0.0;
     int demanda_atendida;
     int custo = 0;
+
+    double indice_construcao = 0.0;
+    bool aprovada = false;
 };
 
 Linha construir_linha(const vector<int>& caminho, Bairro bairros[], int dist[20][20]);
@@ -119,8 +122,14 @@ Linha construir_linha(const vector<int>& caminho, Bairro bairros[], int dist[20]
         }
     }
 
+    linha.indice_construcao = (linha.custo > 0) ? linha.demanda_atendida * linha.media_viabilidade / linha.custo : 0.0;
+
+    if (linha.indice_construcao >= 10) linha.aprovada = true;
+
     return linha;
 }
+
+
 
 int main() {
     // Lista de bairros, viabilidade e fluxo
@@ -173,19 +182,50 @@ int main() {
         /*19*/ { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,11, 0, 0,10, 0, 0, 8, 7, 0 }
     };
 
-    cout << "teste1" << endl;
-
     vector<tuple<int, int, int>> caminhos;
 
     obter_bairros_viaveis(bairros, &size);
 
-    cout << "teste2" << endl;
-
     auto linhas = encontrar_linhas_metro(bairros, size, dist);
 
-    cout << "teste3" << endl;
+    sort(linhas.begin(), linhas.end(), [](const Linha& a, const Linha& b) {
+        return a.indice_construcao > b.indice_construcao;
+    });
 
-    cout << "Total de linhas encontradas: " << linhas.size() << endl << endl;
+    cout << "Bairros Viáveis: " << size << endl << endl;
+
+    for (int i = 0; i < size; i++) {
+        cout << "ID: " << bairros[i].index << " | Nome: " << bairros[i].nome << " | Viabilidade: " << bairros[i].viabilidade << endl;
+    }
+    cout << endl;
+
+    cout << "20 Conexões Recomendadas: " << endl << endl;
+
+    for (int i = 0; i < 20; i++) {
+        const Linha& L = linhas[i];
+
+        cout << "Linha " << i + 1 << ":" << endl;
+
+        cout << "  Percurso: ";
+        for (const auto& p : L.percurso) {
+            int idx_array    = get<0>(p);
+            int idx_original = get<1>(p);
+            cout << bairros[idx_array].nome
+                << "(" << idx_original << ") ";
+        }
+        cout << endl;
+
+        cout << "  Tamanho da linha: " << L.tamanho << endl;
+        cout << "  Soma das viabilidades: " << L.soma_viabilidade << endl;
+        cout << "  Media de viabilidade: " << L.media_viabilidade << endl;
+        cout << "  Demanda atendida: " << L.demanda_atendida << endl;
+        cout << "  Custo da linha: " << L.custo << endl;
+        cout << "  Indice da linha: " << L.indice_construcao << endl;
+
+        cout << "-------------------------------------" << endl;
+    }
+
+    cout << endl << "Total de linhas encontradas: " << linhas.size() << endl << endl;
 
     for (int i = 0; i < linhas.size(); i++) {
         const Linha& L = linhas[i];
@@ -206,9 +246,72 @@ int main() {
         cout << "  Media de viabilidade: " << L.media_viabilidade << endl;
         cout << "  Demanda atendida: " << L.demanda_atendida << endl;
         cout << "  Custo da linha: " << L.custo << endl;
+        cout << "  Indice da linha: " << L.indice_construcao << endl;
 
         cout << "-------------------------------------" << endl;
     }
+
+    cout << "Linhas aprovadas: " << endl << endl;
+    int aprovadas = 0;
+
+    for (int i = 0; i < linhas.size(); i++) {
+        const Linha& L = linhas[i];
+
+        if (L.aprovada == false) {
+            continue;
+        } else {
+            aprovadas++;
+        }
+
+        cout << "Linha " << i + 1 << ":" << endl;
+
+        cout << "  Percurso: ";
+        for (const auto& p : L.percurso) {
+            int idx_array    = get<0>(p);
+            int idx_original = get<1>(p);
+            cout << bairros[idx_array].nome
+                << "(" << idx_original << ") ";
+        }
+        cout << endl;
+
+        cout << "  Tamanho da linha: " << L.tamanho << endl;
+        cout << "  Soma das viabilidades: " << L.soma_viabilidade << endl;
+        cout << "  Media de viabilidade: " << L.media_viabilidade << endl;
+        cout << "  Demanda atendida: " << L.demanda_atendida << endl;
+        cout << "  Custo da linha: " << L.custo << endl;
+        cout << "  Indice da linha: " << L.indice_construcao << endl;
+
+        cout << "-------------------------------------" << endl;
+    }
+
+    cout << endl << "Métricas gerais" << endl << endl;
+
+    cout << "Porcentagem de Bairros aprovados: " << (static_cast<double>(size) / 20) * 100 << "%" << endl;
+    cout << "Quantidade de linhas encontradas: " << linhas.size() << endl;
+    cout << "Porcentagem de linhas aprovadas: " << (static_cast<double>(aprovadas) / linhas.size()) * 100 << "%";
+
+    cout << endl << "Melhor Linha: " << endl << endl;
+
+    cout << "-------------------------------------" << endl << endl;
+
+    cout << "Linha " << 0 << ":" << endl;
+
+    cout << "  Percurso: ";
+    for (const auto& p : linhas[0].percurso) {
+        int idx_array    = get<0>(p);
+        int idx_original = get<1>(p);
+        cout << bairros[idx_array].nome << "(" << idx_original << ") ";
+    }
+    cout << endl;
+
+    cout << "  Tamanho da linha: " << linhas[0].tamanho << endl;
+    cout << "  Soma das viabilidades: " << linhas[0].soma_viabilidade << endl;
+    cout << "  Media de viabilidade: " << linhas[0].media_viabilidade << endl;
+    cout << "  Demanda atendida: " << linhas[0].demanda_atendida << endl;
+    cout << "  Custo da linha: " << linhas[0].custo << endl;
+    cout << "  Indice da linha: " << linhas[0].indice_construcao << endl;
+
+    cout << "-------------------------------------" << endl;
 
     return 0;
 }
